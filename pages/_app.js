994fa2d5e5b511edccd300d9a9943bbc8db97f53
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-23 19:36:14 +0530

    NProgress done...

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,27 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
+
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
+
   return (
     <>
-      <Head>{/* NProgress */}</Head>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />

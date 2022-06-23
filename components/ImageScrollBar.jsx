ee65db4dee6759ab00815ca8dadf994141811d97
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-23 20:45:55 +0530

    small fixes

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -47,7 +47,13 @@ export default function ImageSrollbar({ data }) {
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
-        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Box
+          key={item.id}
+          width="910px"
+          itemId={item.id}
+          overflow="hidden"
+          p="1"
+        >
           <Image
             placeholder="blur"
             blurDataURL={item.url}
@@ -55,6 +61,7 @@ export default function ImageSrollbar({ data }) {
             width={1000}
             height={500}
             sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+            alt={`Item ${item.id}`}
           />
         </Box>
       ))}

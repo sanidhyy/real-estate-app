9a17dcea66380305e0d9145abb29b717d061228d
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-23 10:30:01 +0530

    footer created

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    2021 Realtor, Inc.
+  </Box>
+);
+
+export default Footer;

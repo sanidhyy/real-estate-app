d042559599ab50050d7a8f6902fdc1a984c8a8f5
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-23 11:13:27 +0530

    fetched searched data from api

--- pages/search.js
+++ pages/search.js
@@ -8,8 +8,9 @@ import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
 
 import noResultsImage from "../assets/images/noresult.svg";
+import { baseURL, fetchApi } from "../utils/fetchApi";
 
-const Search = () => {
+const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
@@ -35,11 +36,11 @@ const Search = () => {
         Properties {router.query.purpose}
       </Text>
       <Flex flexWrap="wrap">
-        {[].map((property) => (
+        {properties.map((property) => (
           <Property property={property} key={property.id} />
         ))}
       </Flex>
-      {[].length === 0 && (
+      {properties.length === 0 && (
         <Flex
           justifyContent="center"
           alignItems="center"
@@ -56,5 +57,28 @@ const Search = () => {
     </Box>
   );
 };
+
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || "for-rent";
+  const rentFrequency = query.rentFrequency || "yearly";
+  const minPrice = query.minPrice || "0";
+  const maxPrice = query.maxPrice || "1000000";
+  const roomsMin = query.roomsMin || "0";
+  const bathsMin = query.bathsMin || "0";
+  const sort = query.sort || "price-desc";
+  const areaMax = query.areaMax || "35000";
+  const locationExternalIDs = query.locationExternalIDs || "5002";
+  const categoryExternalID = query.categoryExternalID || "4";
+
+  const data = await fetchApi(
+    `${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+  );
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
 
 export default Search;
